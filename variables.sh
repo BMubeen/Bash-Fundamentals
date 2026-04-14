6276ba5bee09161974b06e4dfd31c5b392b9aa75
BMubeen <burraqm@VMubeen.myguest.virtualbox.org>
2026-04-14 10:27:34 -0500

    making them again

---
+++ variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# CONCEPT: VARIABLES
+# Used to store data that can be used and manipulated throughout your script or command-line session
+# Variables store data so you can reuse it.
+# It works by Assign using = (No spaces), Use $ when using the variable

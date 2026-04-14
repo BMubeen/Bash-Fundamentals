26d0e3cd207011ab88decb350b2197ac81c2b0a5
BMubeen <burraqm@VMubeen.myguest.virtualbox.org>
2026-04-14 10:07:58 -0500

    Explained the trouble shooting and common mistakes also showed an example

--- variables.sh
+++ variables.sh
@@ -2,3 +2,17 @@
 # CONCEPT: VARIABLES
 # Used to store data that can be used and manipulated throughout your script or command-line session
 # Variables store data so you can reuse it.
+# It works by Assign using = (No spaces), Use $ when using the variable
+# Troubleshooting:
+# - Remove spaces around = 
+# - Only use $ when using the variable 
+# Common mistakes: 
+# name = "Burraq" (spaces break it)
+# $name="Burraq"  (Wrong when assigning)
+# Demo : 
+
+name="Burraq"
+age=16
+
+echo "Name: $name"
+echo "Age: $age"

e0dfa65bf69f2bbbd54999321d9395ac3b1b7b01
BMubeen <burraqm@VMubeen.myguest.virtualbox.org>
2026-04-20 10:10:43 -0500

    Explained how it works

--- if_else.sh
+++ if_else.sh
@@ -2,3 +2,10 @@
 # What is it?
 # An if/else statement allows your program to make decisions.
 # It runs different code depending on whether a condition is true or false.
+# How it works:
+# if [ condition ]
+# then
+#   code runs if condition is true
+# else
+#   code runs if condition is false
+# fi

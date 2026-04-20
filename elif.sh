5f665016bc2b6a201c5f32ae12a1167b4cc0d789
BMubeen <burraqm@VMubeen.myguest.virtualbox.org>
2026-04-20 10:21:06 -0500

    Added how it works

--- elif.sh
+++ elif.sh
@@ -4,3 +4,13 @@
 # What is it?
 # elif allows you to check multiple conditions in sequence.
 # It runs the first condition that is true.
+# How it works:
+# if [ condition1 ]
+# then
+#   code
+# elif [ condition2 ]
+# then
+#   code
+# else
+#   code
+# fi

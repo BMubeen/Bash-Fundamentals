92a69b10634058163af31defb9ec0864c2772157
BMubeen <burraqm@VMubeen.myguest.virtualbox.org>
2026-04-28 10:05:11 -0500

    Explained how they work

--- case.sh
+++ case.sh
@@ -2,3 +2,8 @@
 # What is it?
 # A case statement is used to check a variable against multiple possible values.
 # It is cleaner than using many if/elif statements.
+# How it works:
+# case variable in
+#   pattern) command ;;
+#   pattern) command ;;
+# esac

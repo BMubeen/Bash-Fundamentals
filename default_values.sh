591aa46f9dea5296b09f43e689fbb6e2f9973e3f
BMubeen <burraqm@VMubeen.myguest.virtualbox.org>
2026-04-30 10:39:04 -0500

    Explained how defualt values worl

--- default_values.sh
+++ default_values.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
- # What is it?
+
 # Default values allow Bash to assign a backup value
 # when no value is provided.
+# How it works:
+# Bash uses this syntax:
+# ${variable:-default}
+# If variable is empty, Bash uses the default value.

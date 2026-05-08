b59e53d0b01cedbf061a583d281fc4c04ae0a09b
BMubeen <260471042+BMubeen@users.noreply.github.com>
2026-05-08 10:18:16 -0500

    Explained some important factors about do while loops

--- do_while_loops.sh
+++ do_while_loops.sh
@@ -4,6 +4,7 @@
 # It is useful when the number of repetitions is unknown.
 # How it works :
 # Repedeatly executes a block of commands as long as a specific condition evaluates to true 
-# Important:
-# Bash does not have a built-in do-while loop.
+# Important: 
+# Bash does not have a built-in do while loop.
 # It can be simulated using while loops.
+ 

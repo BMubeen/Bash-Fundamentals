7799e14ca18cd025035cdc21d994473d45093a2c
BMubeen <260471042+BMubeen@users.noreply.github.com>
2026-05-08 10:16:06 -0500

    Explained how while loops work

--- do_while_loops.sh
+++ do_while_loops.sh
@@ -2,3 +2,8 @@
 # What is it?
 # A while loop repeats code while a condition is true.
 # It is useful when the number of repetitions is unknown.
+# How it works :
+# Repedeatly executes a block of commands as long as a specific condition evaluates to true 
+# Important:
+# Bash does not have a built-in do-while loop.
+# It can be simulated using while loops.

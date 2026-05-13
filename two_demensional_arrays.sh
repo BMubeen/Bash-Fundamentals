a1bdb5bb1776d420db44ea1c54eddeafd3cf9d07
BMubeen <260471042+BMubeen@users.noreply.github.com>
2026-05-13 10:07:27 -0500

    Explained how bash simulates two dimensional arrays

--- two_demensional_arrays.sh
+++ two_demensional_arrays.sh
@@ -3,3 +3,6 @@
 # What is it? 
 # A two dimensional array stores rows and columns of data.
 # bash Does not support true 2D arrays directly. 
+# How it works :
+# Bash simulates 2D arrays usig 2D arrays of strings.
+# Each string represents a row.

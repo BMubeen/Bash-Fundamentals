9c6df214afe33853f295435e9a7ebd9bc9141a97
BMubeen <260471042+BMubeen@users.noreply.github.com>
2026-05-13 10:33:09 -0500

    explained how pipes work

--- pipes.sh
+++ pipes.sh
@@ -3,3 +3,8 @@
 # What is it? 
 # A pipe sends the outut oof one command
 # Directly into another command.
+# How it works:
+# command1 | command2
+#
+# command1 produces output
+# command2 receives and processes it

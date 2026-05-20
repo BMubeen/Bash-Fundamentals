18bd74e95e0b2a437a3e9bf8f93e2e3c12ab0059
BMubeen <260471042+BMubeen@users.noreply.github.com>
2026-05-20 10:24:06 -0500

    Explained how for loops work and the structure behind it

--- for_loops.sh
+++ for_loops.sh
@@ -2,3 +2,10 @@
 # What is it?
 # A for loop repeats code for a list of items or values.
 # It is useful when the number of repetitions is known.
+# How it works: 
+# - Repeates a code for each time in a list
+# - the loop changes each cycle 
+# Structure for varaible in list:
+# - do 
+# - commands 
+#- done 
